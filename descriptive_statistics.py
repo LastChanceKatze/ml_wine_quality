d58eb825008dc04d65dde239aaeda1c8dbb6c540
Milica Todorovic <last.chance.katze@elfak.rs>
2020-12-21 21:55:09 +0100

    add: bar plots, box plots, correlation matrix

--- descriptive_statistics.py
+++ descriptive_statistics.py
@@ -2,15 +2,8 @@ import preprocess as pp
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-data = pp.import_data()
-print("Describe data: \n" + str(data.describe(include='all')))
-print("----------------------------------------------------------")
-print("Print median: \n" + str(data.median()))
-print("----------------------------------------------------------")
-print("Skewness: \n" + str(data.skew()))
-print("----------------------------------------------------------")
 
-
+# TODO: Remove, unused
 def plot_density_graph(data):
     for column in data.columns:
         fig = plt.figure()
@@ -23,19 +16,85 @@ def plot_density_graph(data):
         plt.savefig("density_" + str(column)+".jpg")
 
 
-def plot_boxplot(data, y_coordinate):
-    sns.set(style="whitegrid")
-    plt.figure(figsize=(10, 8))
-    sns.boxplot(x='quality', y=y_coordinate, data=data)
+def plot_boxplot(data):
+    data.drop('quality', axis=1).plot(kind='box', subplots=True, layout=(2, 6),
+                                      sharex=False, sharey=False,
+                                      title='Box Plot for each input variable')
+    plt.subplots_adjust(wspace=0.5)
+    plt.savefig('box_plot.jpg')
     plt.show()
 
 
-def plot_histogram(data_column):
+# TODO: uses seaborn
+def plot_barplots(data):
+    num_rows = 3
+    num_cols = 4
+    fig, axs = plt.subplots(num_rows, num_cols)
+    plt.subplots_adjust(hspace=0.5, wspace=.5, top=0.95)
+
+    for i in range(0, num_rows):
+        for j in range(0, num_cols):
+            index = j * num_rows + i
+            col_name = data.columns[index]
+            sns.barplot(x='quality', y=col_name, data=data, ax=axs[i][j])
+
+    plt.savefig("bar_plot.jpg")
+    plt.show()
+
+
+def plot_histogram(data, data_column):
     plt.figure(figsize=(14, 8))
-    sns.distplot(data_column, kde=False)
+    data[data_column].plot(kind='hist')
+    # sns.histplot(data_column, kde=True)
     plt.show()
 
 
-# plot_density_graph(data)
-# plot_boxplot(data, y_coordinate="density")
-plot_histogram(data['density'])
+# TODO: uses seaborn
+def correlation_matrix(data):
+    corr = data.corr()
+    sns.heatmap(corr, xticklabels=corr.columns, yticklabels=corr.columns, annot=True)
+    plt.savefig("correlation_matrix.png")
+    plt.show()
+
+    corr = abs(corr['quality']).sort_values(ascending=False)
+    return corr
+
+
+def exploratory_analysis():
+    data = pp.import_data()
+    """
+    outliers can be seen in:
+    free sulfur dioxide, total sulfur dioxide, residual sugar, alcohol, sulphates
+    """
+    print("Describe data: \n" + str(data.describe(include='all')))
+    print("----------------------------------------------------------")
+    print("Print median: \n" + str(data.median()))
+    print("----------------------------------------------------------")
+    print("Skewness: \n" + str(data.skew()))
+    print("----------------------------------------------------------")
+
+    """
+    quality histogram
+    the dataset is unbalanced
+    much more good wines than bad or excellent ones
+    """
+    # plot_histogram(data=data, data_column='quality')
+
+    """
+    box plots for each variable
+    outliers can be seen
+    as well as data range
+    """
+    # plot_boxplot(data)
+
+    """
+    bar plot for each variable by quality
+    """
+    # plot_barplots(data)
+
+    # correlation matrix
+    corr = correlation_matrix(data)
+    print("Sorted correlation values: \n" + str(corr))
+
+
+exploratory_analysis()
