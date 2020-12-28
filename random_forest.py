3ae8473a480480a85593d14b9fccf46f1b596056
Milica Todorovic <last.chance.katze@elfak.rs>
2020-12-28 15:54:26 +0100

    add: evaluate methods to a sepate file

--- random_forest.py
+++ random_forest.py
@@ -1,12 +1,10 @@
 from sklearn.ensemble import RandomForestClassifier
-import sklearn.metrics as metrics
-from sklearn.model_selection import cross_val_score
-import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import seaborn as sns
 import preprocess as pp
+import evaluate as eval
 
+# TODO: add GridSearchCV to improve performance
 
 # get data
 data = pp.import_data()
@@ -18,29 +16,14 @@ model = rfc.fit(x_train, y_train)
 y_pred = rfc.predict(x_test)
 #
 
-# evaluation
-df_y_test = pd.DataFrame(y_test)
-print("Test set class distribution: \n", df_y_test.value_counts())
-# TODO: add cross validation score
-print("Report:\n", metrics.classification_report(y_test, y_pred))
-print('F1 Score:\n', metrics.f1_score(y_test, y_pred, average='micro'))
-print('Precision Score:\n', metrics.precision_score(y_test, y_pred, average="micro"))
-print("Accuracy:\n", metrics.accuracy_score(y_test, y_pred))
-#
+# cross validation score
+eval.cross_val_eval(rfc, x_train, y_train, 5)
+
+# accuracy, recall, precision, f1
+eval.evaluate_model(y_test, y_pred)
 
 # confusion matrix
-conf_matrix = metrics.confusion_matrix(y_test, y_pred)
-print("Confusion matrix:\n", conf_matrix)
-
-class_names = data['quality'].unique()
-df_conf = pd.DataFrame(conf_matrix, index=class_names, columns=class_names)
-sns.heatmap(df_conf, annot=True,  fmt='g')
-plt.title("Confusion matrix: Random Forest")
-plt.xlabel("False")
-plt.ylabel("True")
-plt.savefig("./graphs/confusion_matrix_rfc.jpg")
-plt.show()
-#
+eval.confusion_matrix(y_test, y_pred, data, False)
 
 # important features
 feature_importance = model.feature_importances_
