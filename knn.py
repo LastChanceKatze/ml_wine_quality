6a44b6f6d3593e4c4b215e3e1479f157382b51fa
DESKTOP-DDSK4PA\Nata <natastam0796@gmail.com>
2021-01-11 22:08:53 +0100

    added grid search knn and svm

--- knn.py
+++ knn.py
@@ -1,4 +1,4 @@
-from preprocess import *
+
 import sklearn.metrics as metrics
 #import necessary modules
 from sklearn.neighbors import KNeighborsClassifier
@@ -6,25 +6,45 @@ from sklearn.metrics import accuracy_score
 import matplotlib.pyplot as plt
 from preprocess import *
 from evaluate import *
+from sklearn.model_selection import GridSearchCV
+def grid_search(is_grid_search=False):
 
-data=import_data()
+    data=import_data()
 
-data_train, data_test, target_train, target_test=preprocess(data)
-#create object of the lassifier
-clf = KNeighborsClassifier(n_neighbors=9, metric='manhattan')
-#Train the algorithm
-clf.fit(data_train, target_train)
-# predict the response
-pred = clf.predict(data_test)
-# evaluate accuracy
+    data_train, data_test, target_train, target_test=preprocess(data)
+    #create object of the lassifier
 
-# cross validation score
-cross_val_eval(clf, data_train, target_train, 5)
+    tuned_parameters = [{'n_neighbors': [3,5,11,19,23],
+                        'metric': ['euclidean','manhattan'],
+                        'weights': ['uniform', 'distance']}]
 
-# accuracy, recall, precision, f1
-evaluate_model(target_test, pred)
+    #best params {'metric': 'manhattan', 'n_neighbors': 19, 'weights': 'distance'}
 
-# confusion matrix
-confusion_matrix(target_test, pred, data, False, "KNN")
+    clf=KNeighborsClassifier()
+
+    if is_grid_search:
+        clf=GridSearchCV(clf, tuned_parameters)
+    else:
+        clf=KNeighborsClassifier( metric = 'manhattan', n_neighbors = 19, weights ='distance')
 
 
+    #Train the algorithm
+    clf.fit(data_train, target_train)
+
+    if is_grid_search:
+        print(clf.best_params_)
+        print(pd.concat([pd.DataFrame(clf.cv_results_["params"]),pd.DataFrame(clf.cv_results_["mean_test_score"], columns=["Accuracy"])],axis=1))
+   
+    # predict the response
+    pred = clf.predict(data_test)
+
+    # cross validation score
+    cross_val_eval(clf, data_train, target_train, 5)
+
+    # accuracy, recall, precision, f1
+    evaluate_model(target_test, pred)
+
+    # confusion matrix
+    confusion_matrix(target_test, pred, data, False, "KNN")
+
+grid_search(True)
--- svm.py
+++ svm.py
@@ -4,26 +4,51 @@ from sklearn.metrics import accuracy_score
 import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn import svm
-
+from sklearn.model_selection import GridSearchCV
+import pandas as pd
 from preprocess import *
 from evaluate import *
+from feature_selection import *
 
-#from sklearn.svm import SVC
-#import preprocess
-data=import_data()
+#best params {'C': 1000, 'gamma': 0.001, 'kernel': 'rbf'}
+def grid_search(is_grid_search=False):
 
-data_train, data_test, target_train, target_test = preprocess(data)
-clf = svm.SVC()
+    tuned_parameters = [{'kernel': ['rbf'],
+                     'gamma': [1e-3, 1e-4],
+                     'C': [1, 10, 100, 1000]}]
+    data=import_data()
 
-clf.fit(data_train, target_train)
-pred=clf.predict(data_test)
+    data_train, data_test, target_train, target_test = preprocess(data)
 
-# cross validation score
-cross_val_eval(clf, data_train, target_train, 5)
 
-# accuracy, recall, precision, f1
-evaluate_model(target_test, pred)
+    clf = svm.SVC()
 
-# confusion matrix
-confusion_matrix(target_test, pred, data, False, "svm")
+    if is_grid_search:
+        clf=GridSearchCV(clf, tuned_parameters)
+    else:
+        clf=svm.SVC(C=1000, gamma=0.001, kernel='rbf')
+
+
+    clf.fit(data_train, target_train)
+
+    if is_grid_search:
+        print(clf.best_params_)
+        print(pd.concat([pd.DataFrame(clf.cv_results_["params"]),pd.DataFrame(clf.cv_results_["mean_test_score"], columns=["Accuracy"])],axis=1))
+
+
+
+    pred=clf.predict(data_test)
+
+    #cross validation score
+    cross_val_eval(clf, data_train, target_train, 5)
+
+    # accuracy, recall, precision, f1
+    evaluate_model(target_test, pred)
+
+    # confusion matrix
+    confusion_matrix(target_test, pred, data, False, "svm")
+
+
+grid_search(False)
+
 
